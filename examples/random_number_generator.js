71d1e84f70ffb9d4da70cba56a85d44c680663b2
Source Academy User <No public email provided>
2024-10-20 20:51:44 +0800

    Changes made from Source Academy

--- examples/random_number_generator.js
+++ examples/random_number_generator.js
@@ -2,44 +2,23 @@
 
 
 //Random die generator
-// function throwdie() {
-//     return math_floor(math_random() * 6) + 1;
-// }
+function throwdie() {
+    return math_floor(math_random() * 6) + 1;
+}
 
-// function test_throw_die(counter) {
-//     function test(counter) {
-//         const n = throwdie();
-//         display(stringify(n) + " | " + stringify(counter));
-//         return counter === 1
-//         ? true
-//         : n < 1  || n >= 7
-//         ? false
-//         : test(counter - 1);
-//     }
-//     return test(counter);
-// }
-// test_throw_die(2000);
-
-
-
-// Two digit random number generator
-// function two_digit() {
-//     return math_floor(math_random() * 89) + 10;
-// }
-
-// function test_two_digit(counter, upperbound, lowerbound) {
-//     function test(counter) {
-//         const n = two_digit();
-//         display(stringify(n) + " | " + stringify(counter));
-//         return counter === 1
-//         ? true
-//         : n < lowerbound  || n >= upperbound 
-//         ? false
-//         : test(counter - 1);
-//     }
-//     return test(counter);
-// }
-// test_two_digit(2000, 100, 10);
+function test_throw_die(counter) {
+    function test(counter) {
+        const n = throwdie();
+        display(stringify(n) + " | " + stringify(counter));
+        return counter === 1
+        ? true
+        : n < 1  || n >= 7
+        ? false
+        : test(counter - 1);
+    }
+    return test(counter);
+}
+test_throw_die(2000);
 
 
 //Two digit random number generator
@@ -55,16 +34,11 @@ function test_two_digit(counter, upperbound, lowerbound) {
             const n = two_digit();
             display(stringify(n) + " | " + stringify(counter));
             return n >= lowerbound && n < upperbound && test(counter - 1);
-            // if (n < lowerbound || n >= upperbound) {
-            //     return false;
-            // } else {
-            //     return test(counter - 1);
-            // }
         }
     }
     return test(counter);
 }
-test_two_digit(10, 100, 10);
+test_two_digit(2000, 100, 10);
 
 
 
